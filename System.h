c4fa4846a84ed2f793a6827741acca95213a6629
Balázs <tsbalazs@freemail.hu>
2019-09-29 17:44:00 +0200

    Revert "Change list to map (rm dont work)"
    This reverts commit c7106755053f29c8124ec9174c7fef1c85983d5c.

--- System.h
+++ System.h
@@ -2,26 +2,23 @@
 #define _system_h_
 
 
-//#include "Dir.h"
+#include "Dir.h"
 #include <list>
 #include <iostream>
-#include <map>
 
 class System{
-	//std::list<Dir*> directories;
+	std::list<Dir*> directories;
 	std::string currentFolder;
-	std::map <std::string, std::string> directories;
-	std::map <std::string, std::string>::iterator it;
 	std::string path;
 	public:
 		System();
-		void mkdir(std::string);
+		void mkdir(std::string, std::string);
 		bool alreadyExists(std::string);
-		void rm(std::string);
 		void ls();
 		void cd(std::string);
 		void cdBack();
 		void printCore();
+		std::string getCurrent();
 };
 
 #endif
